d4266d0cbeac5ff14ab73dc0146741515a6fcf47
alex312 <lixuanalex123@163.com>
2017-11-17 11:11:59 +0800

    修改chrome安装脚本

--- chrome-install.sh
+++ chrome-install.sh
@@ -5,7 +5,18 @@ if [ ! -f "/etc/apt/sources.list.d/google-chrome.list" ]; then
     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 fi
 
-command -v google-chrome-stable >/dev/null 2>&1 || {
+function install {
     sudo apt-get update
     sudo apt-get install google-chrome-stable
+
+    grep 'alias chrome=' $HOME/.bashrc
+    if [ $? -eq 0 ]; then
+        sed -i '/alias chrome=/c\alais chrome="x google-chrome"' $HOME/.bashrc
+    else
+        sed -i '$a\alias chrome="x google-chrome"' $HOME/.bashrc
+    fi
+
+    exec "$SHELL"
 }
+
+command -v google-chrome-stable >/dev/null 2>&1 || install
